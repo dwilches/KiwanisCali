62ef8c15cbbb0ebe8c580338a6f61f2547ef0ca3
Daniel Wilches <dwilches@gmail.com>
2017-09-24 21:14:48 -0700

    Added gallery pagination

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,30 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from "lodash";
+
 declare let $;
+
+const NUM_PHOTOS_PER_PAGE = 6;
 
 @Component({
     selector: 'app-gallery',
     templateUrl: './gallery.component.html',
     styleUrls: ['./gallery.component.scss']
 })
-
 export class GalleryComponent implements OnInit {
 
     public photos = [];
+
+    // Calculated values
     public fromPhoto = 0;
-    public toPhoto = 6;
+    public toPhoto = NUM_PHOTOS_PER_PAGE;
+    public pages = [];
+    public currentPage = 1;
 
     constructor() {
-        for (let i = 1; i <= 20 ; i++) {
+        for (let i = 1; i <= 10; i++) {
             this.photos.push(`/assets/gallery/image (${i})`);
         }
+        const numPages = Math.ceil(this.photos.length / NUM_PHOTOS_PER_PAGE);
+        this.pages = _.range(1, numPages + 1);
     }
 
     ngOnInit() {
     }
 
+    public gotoPage(page)
+    {
+        if (page === 'previous') {
+            if (this.currentPage === 1) {
+                return;
+            }
+            page = this.currentPage - 1;
+        }
+        else if (page === 'next') {
+            if (this.currentPage === this.pages.length) {
+                return;
+            }
+            page = this.currentPage + 1;
+        }
+
+        this.currentPage = page;
+
+            this.fromPhoto = (this.currentPage - 1) * NUM_PHOTOS_PER_PAGE;
+        this.toPhoto = this.fromPhoto + NUM_PHOTOS_PER_PAGE;
+    }
+
 }
 
-/*
+
 $(document).ready(function(){
 
     loadGallery(true, 'a.thumbnail');
@@ -76,4 +106,4 @@ $(document).ready(function(){
         });
     }
 });
-*/
+
