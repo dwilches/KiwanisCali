2727585742ca399c24c94af69eae41b5d3cebe18
Angela <abenavides333@gmail.com>
2017-09-24 18:15:52 -0700

    Gallery created.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+declare let $;
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+    selector: 'app-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.scss']
 })
+
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+    public photos = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+        for (let i = 1; i <= 20 ; i++) {
+            this.photos.push(`/assets/gallery/image (${i})`);
+        }
+    }
+
+    ngOnInit() {
+    }
 
 }
+
+$(document).ready(function(){
+
+    loadGallery(true, 'a.thumbnail');
+
+    // This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+        $('#show-previous-image, #show-next-image').show();
+        if (counter_max === counter_current) {
+            $('#show-next-image').hide();
+        } else if (counter_current === 1) {
+            $('#show-previous-image').hide();
+        }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+        let current_image,
+            selector,
+            counter = 0;
+
+        $('#show-next-image, #show-previous-image').click(function(){
+            if ($(this).attr('id') === 'show-previous-image') {
+                current_image--;
+            } else {
+                current_image++;
+            }
+
+            selector = $('[data-image-id="' + current_image + '"]');
+            updateGallery(selector);
+        });
+
+        function updateGallery(selectorTmp) {
+            const $sel = selectorTmp;
+            current_image = $sel.data('image-id');
+            $('#image-gallery-caption').text($sel.data('caption'));
+            $('#image-gallery-title').text($sel.data('title'));
+            $('#image-gallery-image').attr('src', $sel.data('image'));
+            disableButtons(counter, $sel.data('image-id'));
+        }
+
+        if (setIDs === true) {
+            $('[data-image-id]').each(function(){
+                counter++;
+                $(this).attr('data-image-id', counter);
+            });
+        }
+        $(setClickAttr).on('click', function(){
+            updateGallery($(this));
+        });
+    }
+});
