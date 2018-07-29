46c46bb9cdc0c5b69a12baf5a190b3afca46abbf
Daniel Wilches <dwilches@gmail.com>
2018-07-29 13:20:57 -0700

    Add news section and modals bg

--- website/src/app/intro/intro.component.ts
+++ website/src/app/intro/intro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
-  constructor() { }
+    public newsList = [{
+        imagePath: "/assets/one-time/bingo-bailable.jpg",
+        shortTitle: "Bingo bailable en la Tienda de Pedro",
+        fullTitle: "Adquiere tu Bono para nuestro Gran Bingo en la Tienda de Pedro el 4 de agosto de 2018.<br/>Contacto: 3162928090 / 3155792021",
+    },{
+        imagePath: "/assets/one-time/valla-publicitaria.jpg",
+        shortTitle: "Nueva valla publicitaria",
+        fullTitle: "Nuestra Valla Kiwanis Sultana del Valle Invita a apoyar el proyecto de la Sirena:<br/>Jardin Infantil Kiwanis en Cali",
+    }];
 
-  ngOnInit() {
-  }
+    public selectedNews;
 
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    setSelectedPhoto(photoIndex) {
+      this.selectedNews = this.newsList[photoIndex];
+    }
+
+    closeModal() {
+        $('#image-modal').modal('toggle');
+    }
 }
