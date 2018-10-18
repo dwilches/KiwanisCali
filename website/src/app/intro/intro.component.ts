a0e4727a5195ee36accf7fd3bb57dcc8861b730b
Daniel Wilches <dwilches@gmail.com>
2018-10-17 21:36:51 -0700

    Quitar valla publicitaria

--- website/src/app/intro/intro.component.ts
+++ website/src/app/intro/intro.component.ts
@@ -10,10 +10,6 @@ declare var $;
 export class IntroComponent implements OnInit {
 
     public newsList = [{
-        imagePath: "/assets/one-time/bingo-bailable.jpg",
-        shortTitle: "Bingo bailable en la Tienda de Pedro",
-        fullTitle: "Adquiere tu Bono para nuestro Gran Bingo en la Tienda de Pedro el 4 de agosto de 2018.<br/>Contacto: 3162928090 / 3155792021",
-    },{
         imagePath: "/assets/one-time/valla-publicitaria.jpg",
         shortTitle: "Nueva valla publicitaria",
         fullTitle: "Nuestra Valla Kiwanis Sultana del Valle Invita a apoyar el proyecto de la Sirena:<br/>Jardin Infantil Kiwanis en Cali",
